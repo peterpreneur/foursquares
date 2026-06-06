a072e61a6510575c013cd56d517461dd39d88e98
Peter S <peterpreneur@gmail.com>
2026-06-06 00:17:42 -0500

    print board

--- src/main/java/org/peterpreneur/foursquares/Foursquares.java
+++ src/main/java/org/peterpreneur/foursquares/Foursquares.java
@@ -3,7 +3,6 @@
  */
 package org.peterpreneur.foursquares;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -18,24 +17,42 @@ public class Foursquares {
         System.out.println("Welcome to Foursquares!");
 
         // Initiate board
-        int colBoard = 6;
-        int rowBoard = 6;
+        int colBoard = 8;
+        int rowBoard = 8;
+        int maxvalue = colBoard * rowBoard;
+        int digits = String.valueOf(maxvalue).length();
+        String intFormat = "%" + digits + "d";
         int counter = 1;
 
-        int[][] board = new int[colBoard][rowBoard];
-        for (int i = 0; i < colBoard; i++) {
-            Arrays.fill(board[i], counter + i);
-        }
+        // Setup board
+        int[][] initBoard = setupBoard(colBoard, rowBoard, counter);
 
-        // Printout board
-        for (int i = 0; i < colBoard; i++) {
-            for (int j = 0; j < rowBoard; j++) {
-                System.out.print(board[i][j] + "_");
-            }
-            System.out.println();
-        }
+        // Print board
+        printBoard(colBoard, rowBoard, intFormat, initBoard);
 
         // Play game
         // Determine winner
     }
+
+    private static int[][] setupBoard(int colBoard, int rowBoard, int counter) {
+        int[][] board = new int[colBoard][rowBoard];
+        for (int i = 0; i < colBoard; i++) {
+            for (int j = 0; j < rowBoard; j++) {
+                board[i][j] = counter++;
+            }
+        }
+        return board;
+    }
+
+    private static void printBoard(int colBoard, int rowBoard, String intFormat, int[][] board) {
+        for (int i = 0; i < colBoard; i++) {
+            for (int j = 0; j < rowBoard; j++) {
+                System.out.printf("| " + intFormat + " ", board[i][j]);
+
+            }
+            System.out.println("|");
+        }
+
+    }
+
 }
